b1a08eb49996924ce967cb686ff21d75ee58341e
kareenajeetun-art <kareena.jeetun@gmail.com>
2025-11-20 15:15:31 +0400

    week9

--- auth.py
+++ auth.py
@@ -7,7 +7,7 @@ def hash_password (plain_text_password):
  # Generate a salt and hash the password
   salt = bcrypt.gensalt()
   hashed_password =bcrypt.hashpw(password_bytes, salt)
- # Decodethe hash back to a stringto share in a text file
+ # Decode the hash back to a string to share in a text file
   return hashed_password
 
 # Implement the password verification function
